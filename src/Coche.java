858cff3535cd71f065e084fed5bfd878d3d847a9
Samuel Cam <65782952+makore3@users.noreply.github.com>
2025-10-07 13:09:17 +0200

    feat: extend Coche class with new attributes (climatizador, asientosCuero), price/peso calculations, and update UsoCoches usage

--- src/Coche.java
+++ src/Coche.java
@@ -1,19 +1,87 @@
 public class Coche {
 
-    // Método constructor
+    // Constructor Method
     public Coche() {
         ruedas = 4;
         color = "gris";
+        pesoBase = 1350.25;
+        peso = pesoBase;
+        precioBase = 15650.25;
+        precio = precioBase;
     }
 
     // Variables
     private double ancho;
-    public double alto;
+    private double alto;
     private double peso;
+    private double pesoBase;
     private int ruedas;
     private String color;
+    private boolean climatizador;
+    private boolean asientosCuero;
+    private double precioBase;
 
-    // Métodos
+    // Methods
+    public String isClimatizador() {
+        if (climatizador) {
+            return "El coche incorpora climatizador";
+        } else {
+            return "El coche incorpora aire acondicionado";
+        }
+    }
+
+    public void setClimatizador(String climatizador) {
+        if (climatizador.equalsIgnoreCase("si")) {
+            this.climatizador = true;
+        } else {
+            this.climatizador = false;
+        }
+        setPrecio();
+        setPeso();
+    }
+
+    public String isAsientosCuero() {
+        if (climatizador) {
+            return "El coche tiene asientos de cuero";
+        } else {
+            return "El coche no tiene asientos de cuero";
+        }
+    }
+
+    public void setAsientosCuero(String asientosCuero) {
+        if (asientosCuero.equalsIgnoreCase("si")) {
+            this.asientosCuero = true;
+        } else {
+            this.asientosCuero = false;
+        }
+        setPrecio();
+        setPeso();
+    }
+
+    public double getPrecioBase() {
+        return precioBase;
+    }
+
+    public void setPrecioBase(double precioBase) {
+        this.precioBase = precioBase;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    private void setPrecio() {
+        if (asientosCuero) {
+            precioBase += 3500;
+        }
+        if (climatizador) {
+            precioBase += 3250.20;
+        }
+        precio = precioBase;
+    }
+
+    private double precio;
+
     public double getAncho() {
         return ancho;
     }
@@ -34,8 +102,22 @@ public class Coche {
         return peso;
     }
 
-    public void setPeso(double peso) {
-        this.peso = peso;
+    private void setPeso() {
+        if (asientosCuero) {
+            pesoBase += 50;
+        }
+        if (climatizador) {
+            pesoBase += 70;
+        }
+        peso = pesoBase;
+    }
+
+    public double getPesoBase() {
+        return pesoBase;
+    }
+
+    public void setPesoBase(double pesoBase) {
+        this.pesoBase = pesoBase;
     }
 
     public String getColor() {
@@ -47,7 +129,7 @@ public class Coche {
     }
 
     public void setRuedas(int ruedas) {
-        if(ruedas<3 || ruedas>8) {
+        if (ruedas < 3 || ruedas > 8) {
             System.out.println("El nro de ruedas no puede ser ese");
         } else {
             this.ruedas = ruedas;
@@ -61,9 +143,11 @@ public class Coche {
     void arrancar() {
 
     }
+
     void frenar() {
 
     }
+
     void girar() {
 
     }
--- src/UsoCoches.java
+++ src/UsoCoches.java
@@ -2,23 +2,11 @@ public class UsoCoches {
 
     public static void main(String[] args) {
 
-        Coche renault = new Coche();
+        Coche coche1 = new Coche();
 
-        renault.alto = 1520.3;
-        renault.arrancar();
-        renault.setPeso(1250.3);
+        coche1.setAsientosCuero("si");
 
-        Coche mazda = new Coche();
-
-        mazda.setColor("blanco");
-        mazda.setRuedas(3);
-        mazda.girar();
-        mazda.setPeso(1350.2);
-
-        /*
-        System.out.println("El mazda tiene: " + mazda.getRuedas() + " ruedas.");
-        System.out.println("El renault tiene: " + renault.getRuedas() + " ruedas.");
-         */
-        System.out.println("El mazda es de color: " + mazda.getColor() + ".");
+        System.out.println("Peso: " + coche1.getPeso());
+        System.out.println("Precio: " + coche1.getPrecio());
     }
 }
